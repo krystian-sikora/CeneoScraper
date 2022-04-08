7801f3c450d56df2180697b97456f6b87a9c44fd
krystian-sikora <s223097@student.uek.krakow.pl>
2022-04-08 11:14:29 +0200

    2022-04-08 KS

--- scraper.py
+++ scraper.py
@@ -17,3 +17,9 @@ content = opinion.select_one("div.user-post__text").get_text()
 pros = opinion("div[class$=\"positives\"]") # ma być lista zalet, do zrobienia
 print(pros)
 
+useful = opinion.select_one("button.vote-yes > span").get_text().strip
+useless = opinion.select_one("button.vote-no > span").get_text().strip
+publish_date = opinion.select_one("span.user-post_published > time:nth-child(1)")["datetime"]
+purchase_date = opinion.select_one("span.user-post_published > time:nth-child(2)")["datetime"]
+
+print(recommendation, stars, content, useful, useless, publish_date, purchase_date, sep="\n")
