ab6e24503e8162eff035d01be604cd870467f7b4
krystian-sikora <s223097@student.uek.krakow.pl>
2022-05-13 00:19:36 +0200

    2022-05-06 KS

--- README.md
+++ README.md
@@ -6,13 +6,26 @@
 |--------|--------|--------------|------------|
 |opinia|div.js_product-reviev|opinion||
 |identyfikator opinii|div.js_product-reviev\["data-entry-id"\]|opinion_id||
-|autor opinii|span.user-post__author-name|author||
+|autor opinii|span.user-post__author-name|author|str|
 |rekomendacja|span.user-post__author-recomendation > em|recommendation||
 |liczba gwiazdek|span.user-post__score-count|stars||
 |treść opinii|div.user-post__text|content||
-|lista zalet|div[class$="positives"]~ div.reviev-feature__item|pros||
-|lista wad|div[class$="negatives"]~ div.reviev-feature__item|cons||
-|dla ilu osób przydatna|span[id^="votes-yes"]|useful||
-|dla ilu osób nieprzydatna|span[id^="votes-no"]|useless||
-|data wystawienia opinii|span.user-post__published > time:nth-child(1)["datetime"]|publish_date||
-|data zakupu|span.user-post__published > time:nth-child(2)["datetime"]|purchase_date||
+|lista zalet|div.review-feature__title--positives ~ div.review-feature__item <br> div\[class$=positives\] ~ div.review-feature__item<br> div.review-feature__col:has(> div\[class$=positives\]) > div.review-feature__item|pros||
+|lista wad|div.review-feature__title--negatives ~ div.review-feature__item <br> div\[class$=negatives\] ~ div.review-feature__item<br> div.review-feature__col:has(> div\[class$=negatives\]) > div.review-feature__item|cons||
+|dla ilu osób przydatna|span\[id^=votes-yes\]<br>button.vote-yes > span<br>button.vote-yes["data-total-vote"]|useful||
+|dla ilu osób nieprzydatna|span\[id^=votes-no\]<br>button.vote-no > span<br>button.vote-no["data-total-vote"]|useless||
+|data wystawienia mopinii|span.user-post__published > time:nth-child(1)\["datetime"\]|publish_date||
+|data zakupu|span.user-post__published > time:nth-child(2)\["datetime"\]|purchase_date||
+
+## Etapy pracy nad projektem
+1. Pobranie do pojedynczych zmiennych składowych pojedynczej opinii
+2. Zapisanie wszystkich składowych pojedynczej opinii do słownika
+3. Pobranie wszystkich opinii z pojedynczej strony do słowników i zapisanie ich na liście
+4. Zapisanie wszystkich opinii z listy do pliku .json
+5. Pobranie wszystkich opinii o produkcie i zapisanie ich na liście w postaci słowników
+6. Dodanie możliwości podania kodu produktu przez użytkownika 
+7. Optymalizacja kodu
+    a. utworzenie funkcji do ekstrakcji elementów strony
+    b. utworzenie słownika selektorów
+    c. użycie dictionary comprehension do pobrania składowych pojedynczej opinii na podstawie słownika selektorów
+8. 
--- scraper.py
+++ scraper.py
@@ -1,60 +1,48 @@
-from typing import Type
-import requests
 import json
-from textwrap import indent
-from os import sep
 from bs4 import BeautifulSoup
 
-item_id = input("Enter item id: ")
-url = "https://www.ceneo.pl/" + item_id + "#tab=reviews"
+def get_item(ancestor, selector, attribute=None, return_list=False):
+    try:
+        if return_list:
+            return [item.get_text().strip() for item in ancestor.select(selector)]
+        if attribute:
+            return ancestor.select_one(selector)[attribute]
+        return ancestor.select_one(selector).get_text().strip()
+    except (AttributeError, TypeError):
+        return None
+
+selectors = {
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recomendation > em"],
+    "stars": ["span.user-post__score-count"],
+    "content": ["div.user-post__text"],
+    "useful": ["button.vote-yes > span"],
+    "useless": ["button.vote-no > span"],
+    "publish_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
+    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"],
+    "pros": ["div[class$=positives]~ div.review-feature__item", None, True],
+    "cons": ["div[class$=negatives]~ div.review-feature__item", None, True]
+}
+
+product_id = input("Podaj identyfikator produktu: ")
+url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
 all_opinions = []
 while(url):
     response = requests.get(url)
     page = BeautifulSoup(response.text, 'html.parser')
     opinions = page.select("div.js_product-review")
     for opinion in opinions:
-        opinion_id = opinion["data-entry-id"]
-        author = opinion.select_one("span.user-post__author-name").get_text().strip()
-        try:
-            recommendation = opinion.select_one("span.user-post__author-recomendation > em").get_text().strip()
-        except AttributeError:
-            recommendation = None
-        stars = opinion.select_one("span.user-post__score-count").get_text().strip()
-        content = opinion.select_one("div.user-post__text").get_text().strip()
-        useful = opinion.select_one("button.vote-yes > span").get_text().strip()
-        useless = opinion.select_one("button.vote-no > span").get_text().strip()
-        publish_date = opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"]
-        try:
-            purchase_date = opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"]
-        except TypeError:
-            purchase_date: None
-        pros = opinion.select("div[class$=\"positives\"]~ div.review-feature__item")
-        pros = [item.get_text().strip() for item in pros]
-        cons = opinion.select("div[class$=\"negatives\"]~ div.review-feature__item")
-        cons = [item.get_text().strip() for item in cons]
-
         single_opinion = {
-            "opinion_id": opinion_id,
-            "author": author,
-            "recommendation": recommendation,
-            "stars": stars,
-            "content": content,
-            "useful": useful,
-            "useless": useless,
-            "publish_date": publish_date,
-            "purchase_date": purchase_date,
-            "pros": pros,
-            "cons": cons,
+            key:get_item(opinion, *value)
+                for key, value in selectors.items()
         }
+        single_opinion["opinion_id"] = opinion["data-entry-id"]
         all_opinions.append(single_opinion)
 
-    try:
-        url = "https://www.ceneo.pl" + page.select_one("a.pagination__next")["href"]
+    try:    
+        url = "https://www.ceneo.pl"+get_item(page,"a.pagination__next","href")
     except TypeError:
         url = None
 
-with open("opinions/" + item_id + ".json", "w", encoding="UTF-8") as jf:
+with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
     json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
-
-
-    # Zadanie domowe - wpisywanie przez użytkownika id przedmiotu
